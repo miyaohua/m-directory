012566efd14d1b04074a61f4a5597f683431c348
unknown <2542571191@qq.com>
2024-01-10 22:55:15 +0800

    feat:添加目录

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 /**
  * 文章目录自动生成
- * @author 米
+ * @author 米耀华
  */
 export default class Mdirectory {
   defaultOption = {
     // 文章内容容器id
-    contentEle: "",
+    contentEle: "m-content",
     // 目录容器id
-    directoryEle: "",
+    directoryEle: "m-directory",
     // 非过滤目录元素
     filterate: ["h1", "h2", "h3", "h4", "h5", "h6"],
+    filterateClass: {
+      'h1': 'm-directory-1',
+      'h2': 'm-directory-2',
+      'h3': 'm-directory-3',
+      'h4': 'm-directory-4',
+      'h5': 'm-directory-5',
+      'h6': 'm-directory-6',
+    }
   };
   constructor(option) {
     this.defaultOption = option;
@@ -22,13 +30,39 @@ export default class Mdirectory {
    * 初始化方法
    */
   init() {
-    const { contentEle, directoryEle, filterate } = this.defaultOption;
-    let filterateStr = filterate.join();
+    const { contentEle, directoryEle, filterate, filterateClass } = this.defaultOption;
     // 标签内容
-    let html = document
-      .getElementById(contentEle)
-      .querySelectorAll(filterateStr);
-    console.log(html);
-    // 过滤节点
+    let hArr = this.getHEle(contentEle, filterate)
+    if (!hArr.length) {
+      throw new Error(`没有找到符合的元素,当前检测元素为：${filterate}`)
+    }
+    hArr = Array.from(hArr).map(v => {
+      let div = document.createElement('div')
+      // 添加类名
+      Object.keys(filterateClass).forEach(item => {
+        if (v.outerHTML.includes(item)) {
+          v.className = filterateClass[item]
+          div.className = v.className;
+          div.innerText = v.innerText;
+        }
+      })
+      return {
+        ele: div,
+        text: div.innerText
+      }
+    })
+    let directoryElement = document.getElementById(directoryEle)
+    console.log(directoryElement)
+    hArr.forEach(item => {
+      directoryElement.appendChild(item.ele)
+    })
+  }
+
+  /**
+   * 获取内容元素h标签
+   */
+  getHEle(contentEle, filterate) {
+    let filterateStr = filterate.join();
+    return document.getElementById(contentEle).querySelectorAll(filterateStr);
   }
 }
